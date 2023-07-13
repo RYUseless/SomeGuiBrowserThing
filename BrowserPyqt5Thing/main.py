207c355c29397999f9b84f86e9f8397e6c2aadc3
Ryuseless <janfilipzacek@seznam.cz>
2023-07-13 17:13:49 +0200

    getting started

--- BrowserPyqt5Thing/main.py
+++ BrowserPyqt5Thing/main.py
@@ -5,27 +5,46 @@ I am running OS linux, with gnome and some extensions (unite and other stuff)
 Resolution I am able to test window scaling etc on are: 4k, 1440p (13inch) and fullhd(24 or 27 inch)
 -ryuseless
 """
-from PyQt5.QtWidgets import *
 import sys
+from PyQt5.QtWidgets import QVBoxLayout,QMainWindow,QApplication,QLabel,QWidget
+from PyQt5.QtGui import QPixmap, QPalette
+from PyQt5.QtCore import Qt
 
-
-class MainWindow(QMainWindow):
+class MainWindow(QWidget):
     def __init__(self):
         super(MainWindow, self).__init__()
 
+        self.initUI()
+
+    def initUI(self):
+        label1 = QLabel(self)
+
+        label1.setText("<font color=yellow>EPIC GUGL FROM WISH</font>")
+        label1.setAutoFillBackground(True)
+        palette = QPalette()
+        palette.setColor(QPalette.Window,Qt.red)
+        label1.setPalette(palette)
+        label1.setAlignment(Qt.AlignCenter)
+
+        vbox = QVBoxLayout()
+        vbox.addWidget(label1)
+        self.setLayout(vbox)
+
+        self.setWindowTitle('Gugl z wishe')
+
         self.resolutionHeight = QApplication.desktop().screenGeometry().height()
         self.resolutionWidth = QApplication.desktop().screenGeometry().width()
-        appWidth = (int)(self.resolutionWidth*0.5)
-        appHeight = (int)(self.resolutionHeight*0.5)
-        self.setGeometry((int)(self.resolutionWidth/2-appWidth/2), (int)(self.resolutionHeight/2-appHeight/2),appWidth,appHeight)  # (xpos,ypos,width,height)
+
+        appHeight = (int)(self.resolutionHeight * 0.5) #APPHEIGHT value
+        appWidth = (int)(self.resolutionWidth * 0.5) #APPWIDTH value
+
+        self.setGeometry(self.resolutionWidth // 2 - appWidth // 2, self.resolutionHeight // 2 - appHeight // 2, appWidth, appHeight)  #(xpos,ypos,width,height)
         self.showMaximized()
 
-        print("Fullass monitor:",self.resolutionWidth,"x",self.resolutionHeight,"\nPolovina monitoru:",(int)(self.resolutionWidth/2),"x",(int)(self.resolutionHeight/2))
-        #geometry is there for when the window is not in fulscreen, so the resolution is not like 30x30, dont know what it will do with scaling etc
 
 
-
-app = QApplication(sys.argv)
-QApplication.setApplicationName("Gugl z wishe")
-window = MainWindow()
-app.exec_()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    main = MainWindow()
+    main.show()
+    sys.exit(app.exec_())
